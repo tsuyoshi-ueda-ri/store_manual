86d68af3265a8dd75e3017e41a0be530626688bf
tsuyoshi-ueda-ri <63494238+tsuyoshi-ueda-ri@users.noreply.github.com>
2021-01-16 01:13:45 +0900

    view_create

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+import '@fortawesome/fontawesome-free/js/all';
 import * as Trix from "trix";
 import '../src/application.css';
 import 'bootstrap';
