2f79f9d3247fbfee438c8b1a493848ae6d07157a
tsuyoshi-ueda-ri <63494238+tsuyoshi-ueda-ri@users.noreply.github.com>
2021-01-15 11:47:43 +0900

    complete_main_function

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -28,7 +28,7 @@ class MoviesController < ApplicationController
 
     respond_to do |format|
       if @movie.save
-        format.html { redirect_to @movie, notice: 'Movie was successfully created.' }
+        format.html { redirect_to @movie, notice: '投稿が完了しました' }
         format.json { render :show, status: :created, location: @movie }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class MoviesController < ApplicationController
   def update
     respond_to do |format|
       if @movie.update(movie_params)
-        format.html { redirect_to @movie, notice: 'Movie was successfully updated.' }
+        format.html { redirect_to @movie, notice: '更新しました' }
         format.json { render :show, status: :ok, location: @movie }
       else
         format.html { render :edit }
@@ -56,7 +56,7 @@ class MoviesController < ApplicationController
   def destroy
     @movie.destroy
     respond_to do |format|
-      format.html { redirect_to movies_url, notice: 'Movie was successfully destroyed.' }
+      format.html { redirect_to movies_url, notice: '削除しました' }
       format.json { head :no_content }
     end
   end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,13 @@ import * as Trix from "trix";
 import '../src/application.css';
 import 'bootstrap';
 import '../stylesheets/application';
+var Trix = require("trix")
+require("@rails/actiontext")
+
+Trix.config.blockAttributes.default = {
+  tagName: "p",
+  breakOnReturn: true
+}
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
