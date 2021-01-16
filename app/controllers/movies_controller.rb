6cb0f90ab7dd804c01d0783ed5b99e6832f24912
tsuyoshi-ueda-ri <63494238+tsuyoshi-ueda-ri@users.noreply.github.com>
2021-01-16 16:40:49 +0900

    add_to_order-method

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,7 +5,7 @@ class MoviesController < ApplicationController
   # GET /movies
   # GET /movies.json
   def index
-    @movies = Movie.includes(:user)
+    @movies = Movie.includes(:user).order("created_at DESC")
   end
 
   # GET /movies/1
