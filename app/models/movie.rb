92881b1f09a7fcf08c18d27ec50148e9cd0e9b45
tsuyoshi-ueda-ri <63494238+tsuyoshi-ueda-ri@users.noreply.github.com>
2021-01-18 01:04:01 +0900

    view_arrange

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,7 +3,7 @@ class Movie < ApplicationRecord
   # :moveはファイルの呼び名 Active Storageは裏側でBlobとAttachmentモデルを使ってMovie.movieを使えるようにしてくれる。
   has_many :comments, foreign_key: :movie_id, dependent: :destroy
   validates :movie,:title, presence: true
-  belongs_to :user
+  belongs_to :user, optional: true
 
   def self.search(search)
     if search != ""
