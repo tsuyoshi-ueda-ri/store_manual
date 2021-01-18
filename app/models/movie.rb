a713489f137b9d22bfab98ac7d1f70a428eddb92
tsuyoshi-ueda-ri <63494238+tsuyoshi-ueda-ri@users.noreply.github.com>
2021-01-18 10:23:05 +0900

    Update movie.rb
    movie.rbにtagとtagmapsへのassociation記述

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,9 +1,12 @@
 class Movie < ApplicationRecord
   has_one_attached :movie
-  # :moveはファイルの呼び名 Active Storageは裏側でBlobとAttachmentモデルを使ってMovie.movieを使えるようにしてくれる。
+  # Active Storageは裏側でBlobとAttachmentモデルを使ってMovie.movieを使えるようにしてくれる。
   has_many :comments, foreign_key: :movie_id, dependent: :destroy
   validates :movie,:title, presence: true
   belongs_to :user, optional: true
+  has_many :tagmaps, dependent: :destroy
+  has_many :tags, through: :tagmaps
+
 
   def self.search(search)
     if search != ""
